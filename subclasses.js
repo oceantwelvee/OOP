8ee5545b7c28db19afd4756768a01cedc3ad77ae
oceantwelvee <90316104+oceantwelvee@users.noreply.github.com>
2022-09-21 19:32:31 +0400

    Update subclasses.js

--- subclasses.js
+++ subclasses.js
@@ -1,1 +1,29 @@
 // Subclasses;
+
+
+class Book {
+    constructor(title,author,year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        // ключевое слово this - работает только внутри constructor;
+    }
+
+    getSummary() {
+        return `${this.title} was writen by ${this.author} in ${this.year}`
+    }
+
+}
+
+// Magazine Subclasses;
+class Magazine extends Book {
+    constructor(title, author, year,month) {
+        super(title, author, year);
+        this.month = month;
+    }
+}
+
+// Instantiate Magazine;
+const mag1 = new Magazine("Mag One", "John Doe", "2018", "Jan");
+console.log(mag1.getSummary());
+
