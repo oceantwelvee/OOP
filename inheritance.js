081c145f8788f2b61252f2c12e8cad68ee720fe7
oceantwelvee <90316104+oceantwelvee@users.noreply.github.com>
2022-09-21 18:54:42 +0400

    Update inheritance.js

--- inheritance.js
+++ inheritance.js
@@ -1,1 +1,33 @@
 // Inheritance - Наследование....
+
+function Book(title,author,year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+
+}
+
+//getSummary;
+Book.prototype.getSummary = function() {
+    return `${this.title} was writen by ${this.author} in ${this.year}`
+}
+
+
+
+//Magazine Constructor;
+function Magazine(title,author,year,month) {
+    Book.call(this,title,author,year);
+
+    this.month = month;
+}
+// Inherit Prototype;
+Magazine.prototype = Object.create(Book.prototype);
+
+// Instantiate Magazine;
+const mag1 = new Magazine("One Mag", "John Doe", "2018", "Jan");
+
+// Use Magazine Constructor;
+
+Magazine.prototype.constructor = Magazine;
+
+console.log(mag1);
