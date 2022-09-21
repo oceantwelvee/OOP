452e1b9ef00fa52761c4503c76bad4ce76f1e3c3
oceantwelvee <90316104+oceantwelvee@users.noreply.github.com>
2022-09-21 19:06:05 +0400

    Update object-create.js

--- object-create.js
+++ object-create.js
@@ -1,1 +1,25 @@
-// Object Create;
+//  Object Protos;
+
+const bookProtos = {
+    getSummary: function() {
+        return `${this.title} was writen by ${this.author} in ${this.year}`
+    },
+    getAge: function() {
+        const years = new Date().getFullYear() - this.year;
+             return `${this.title} is ${years} years old`;
+    }
+}
+
+// Create Object;
+// const book1 = Object.create(bookProtos);
+// book1.title = "One Book";
+// book1.author = "John Doe";
+// book1.year = "2018";
+
+const book1 = Object.create(bookProtos, {
+    title: {value: "One Book"}, 
+    author: {value: "John Doe"},
+    year: {value: "2018"}
+});
+
+console.log(book1);
