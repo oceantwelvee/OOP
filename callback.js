93fd81625939a4d01c1ada7f55f62b6e63366977
oceantwelvee <90316104+oceantwelvee@users.noreply.github.com>
2022-09-22 12:53:30 +0400

    Update and rename async.js to callback.js

---
+++ callback.js
@@ -0,0 +1,28 @@
+// callback
+const posts = [
+    {title: "Post One", body: "This is post One"},
+    {title: "Post Two", body: "This is post Two"},
+];
+
+
+function getPosts() {
+    setTimeout(() => {
+        let output = '';
+        posts.forEach((post,index) => {
+            output += `<li>${post.title}</li>`
+        })
+        document.body.innerHTML = output;
+    },1000);
+}
+
+function createPost(post,callback) {
+    setTimeout(()=> {
+        posts.push(post)
+        callback()
+    },2000)
+
+}
+
+getPosts();
+
+createPost({title: "Post Three", body: "This is post Three"},getPosts);
